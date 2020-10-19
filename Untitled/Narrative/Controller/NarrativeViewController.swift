e7034deb8888b1b00992c20c0bb2c165a18985a0
mariaranha <marina.aranha3@gmail.com>
2020-10-19 15:11:23 -0300

    finish structure

--- Untitled/Narrative/Controller/NarrativeViewController.swift
+++ Untitled/Narrative/Controller/NarrativeViewController.swift
@@ -14,6 +14,13 @@ class NarrativeViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var narrativeSlide: UIImageView!
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var indexPageStackView: UIStackView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    var labels: [UILabel] = []
     
     // MARK: Class Variables
     var numPages: Int = 5
@@ -29,41 +36,14 @@ class NarrativeViewController: UIViewController, UIScrollViewDelegate {
         
         //setup view
         self.view.backgroundColor = AppColor.lightBackground.value
-//        narrativeView.contentView.backgroundColor = AppColor.lightBackground.value
-//        narrativeTitleView.titleLabel.text = "capítulo um"
-//        narrativeTitleView.titleLabel.textColor = AppColor.intermediateLightText.value
-//        narrativeTitleView.subtitleLabel.text = "uma história de carnaval"
-//        narrativeTitleView.imageView.image = UIImage(named: "narrative_star")
         
         //Setup pages
         pageIndex = 0
 //        pages = createPages()
         setupPageScrollView(pages: pages)
+        labels  = [label1, label2, label3, label4, label5]
+//        narrativeView = Bundle.main.loadNibNamed("NarrativeView", owner: self, options: nil)?.first as! NarrativeView
     }
-    
-//    func createPages() -> [NarrativeView] {
-//
-//        for pageNumber in 0...numPages {
-//            let page: NarrativeView = Bundle.main.loadNibNamed("NarrativeView", owner: self, options: nil)?.first as! NarrativeView
-//
-//            switch pageNumber {
-//            case 0:
-//                page.backgroundColor = AppColor.darkBackground.value
-//            case 1:
-//                page.backgroundColor = AppColor.darkBackground.value
-//            case 2:
-//                page.backgroundColor = AppColor.lightBackground.value
-//            case 3:
-//                page.backgroundColor = AppColor.darkBackground.value
-//            default:
-//                break
-//            }
-//
-//            pages.append(page)
-//        }
-//
-//        return pages
-//    }
     
     // MARK: ScrollView
     func setupPageScrollView(pages: [NarrativeView]){
@@ -73,19 +53,7 @@ class NarrativeViewController: UIViewController, UIScrollViewDelegate {
         scrollView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
         scrollView.contentSize = CGSize(width: screenWidth, height: screenHeight * CGFloat(numPages))
         scrollView.isPagingEnabled = true
-//        scrollView.showsVerticalScrollIndicator = false
 
-        
-//        view.layoutSubviews()
-//        for i in 0 ..< pages.count {
-//            pages[i].frame = CGRect(x: narrativeView.frame.width * CGFloat(i),
-//                                       y: narrativeView.frame.minY,
-//                                       width: narrativeView.frame.width,
-//                                       height: narrativeView.frame.height)
-//
-//
-//            scrollView.addSubview(pages[i])
-//        }
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
@@ -97,26 +65,54 @@ class NarrativeViewController: UIViewController, UIScrollViewDelegate {
         case 0:
             narrativeView.backgroundColor = AppColor.lightBackground.value
             playButton.isHidden = true
+            narrativeSlide.isHidden = false
+            setLabels()
+            label1.text = "- I"
+            label1.font = UIFont.boldSystemFont(ofSize: 20.0)
+            narrativeView.imageView.image = UIImage(named: "chapter1_01")
         case 3:
-            narrativeView.backgroundColor = AppColor.darkBackground.value
+            narrativeView.backgroundColor = AppColor.lightBackground.value
             narrativeSlide.isHidden = true
             playButton.isHidden = true
+            setLabels()
+            label2.text = "- II"
+            label2.font = UIFont.boldSystemFont(ofSize: 20.0)
+            narrativeView.imageView.image = UIImage(named: "chapter1_02")
         case 5:
             narrativeView.backgroundColor = AppColor.lightBackground.value
             narrativeSlide.isHidden = true
             playButton.isHidden = true
+            setLabels()
+            label3.text = "- III"
+            label3.font = UIFont.boldSystemFont(ofSize: 20.0)
+            narrativeView.imageView.image = UIImage(named: "chapter1_03")
         case 7:
-            narrativeView.backgroundColor = AppColor.darkBackground.value
+            narrativeView.backgroundColor = AppColor.lightBackground.value
             narrativeSlide.isHidden = true
             playButton.isHidden = true
+            setLabels()
+            label4.text = "- IV"
+            label4.font = UIFont.boldSystemFont(ofSize: 20.0)
+            narrativeView.imageView.image = UIImage(named: "chapter1_03")
         case 9:
             narrativeView.backgroundColor = AppColor.lightBackground.value
             narrativeSlide.isHidden = true
             playButton.isHidden = false
+            setLabels()
+            label5.text = "- V"
+            label5.font = UIFont.boldSystemFont(ofSize: 20.0)
+            narrativeView.imageView.image = UIImage(named: "chapter1_03")
         default:
             break
         }
         
+    }
+    
+    func setLabels(){
+        for elem in labels{
+            elem.font = UIFont.systemFont(ofSize: 20.0)
+            elem.text = "-"
+        }
     }
     
 
--- Untitled/Narrative/View/NarrativeView.swift
+++ Untitled/Narrative/View/NarrativeView.swift
@@ -9,24 +9,24 @@ import UIKit
 
 class NarrativeView: UIView {
     
-//    @IBOutlet var contentView: UIView!
-//    
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        commonInit()
-//    }
-//    
-//    required init?(coder aDecoder: NSCoder){
-//        super.init(coder: aDecoder)
-//        commonInit()
-//    }
-//    
-//    private func commonInit(){
-//        Bundle.main.loadNibNamed("NarrativeView", owner: self, options: nil)
-//        addSubview(contentView)
-//        contentView.frame = self.bounds
-//        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-//        
-//    }
-
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("NarrativeView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+    }
 }
