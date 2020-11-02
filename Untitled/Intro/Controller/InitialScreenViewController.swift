5562b75e472aca2e98fc19266e6baa5068479af5
mariaranha <marina.aranha3@gmail.com>
2020-11-01 21:07:39 -0300

    assets added to initial screen

--- Untitled/Intro/Controller/InitialScreenViewController.swift
+++ Untitled/Intro/Controller/InitialScreenViewController.swift
@@ -8,28 +8,17 @@
 import UIKit
 
 class InitialScreenViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var subtitleLabel: UILabel!
-    @IBOutlet weak var backgroundImage: UIImageView!
-    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupViews()
-        backgroundImage.image = UIImage(named: "initialScreen_background")
         self.view.backgroundColor = AppColor.intermediateBackground.value
-    }
-    
-    //View
-    func setupViews(){
-        //labels
-        titleLabel.text = "untitled games"
-        subtitleLabel.text = "apresenta:"
-    }
-    
-    
+//        backgroundView.backgroundColor = UIColor(patternImage: UIImage(named: "initialScreen_background")!)
 
+    }
+    
+    
+    
+ 
 }
 
