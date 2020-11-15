eabf11eee464224565707b60eac3ce7437a73a90
mariaranha <marina.aranha3@gmail.com>
2020-11-14 21:17:34 -0300

    add settings to board

---
+++ Untitled/Game/Controller/BoardSettingsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BoardSettingsViewController.swift
+//  Untitled
+//
+//  Created by Marina Miranda Aranha on 14/11/20.
+//
+
+import UIKit
+
+class BoardSettingsViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var tutorialButton: UIButton!
+    @IBOutlet weak var exitChapterButton: UIButton!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.textColor = AppColor.highlightText.value
+        subtitleLabel.textColor = AppColor.highlightText.value
+        tutorialButton.setTitleColor(AppColor.intermediateLightText.value, for: .normal)
+        exitChapterButton.setTitleColor(AppColor.intermediateLightText.value, for: .normal)
+        
+    }
+
+    @IBAction func exitChapterButtonTapped(_ sender: Any) {
+        let view = ExitView()
+        view.tag = 100
+        view.delegate = self
+        view.alpha = 0.0
+        self.view.addSubview(view)
+        view.dismiss = {
+            self.performSegue(withIdentifier: "settingsToChaptersMenu", sender: self)
+        }
+        view.cancel = {
+            UIView.animate(withDuration: 0.5, animations: {
+                view.alpha = 0.0
+            }) { (completion) in
+                view.removeFromSuperview()
+            }
+        }
+        UIView.animate(withDuration: 0.5, animations: {
+            view.alpha = 1.0
+        })
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
